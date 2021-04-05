98931bd87f3bd41c98ef4323f35354838d5ff7e6
Craig England <craig.england@krystal.uk>
2021-04-05 09:43:23 +0100

    tweak: copy on cookie banner and version bump

--- src/components/cookieBanner.js
+++ src/components/cookieBanner.js
@@ -2,7 +2,7 @@ export const cookieBanner = `
   <div class="crumbs-banner">
     <div class="crumbs-banner__content">
       <h4 class="crumbs-banner__title">We use cookies</h4>
-      <p class="crumbs-banner__description">By clicking “Accept All Cookies”, you agree to the storing of cookies on your device to enhance site navigation, analyze site usage, and assist in our marketing efforts.</p>
+      <p class="crumbs-banner__description">By clicking “Accept all cookies”, you agree to the storing of cookies on your device to enhance site navigation, analyse site usage, and assist in our marketing efforts. <a href="/privacy">Cookie Policy</a></p>
     </div>
     <div>
       <button class="crumbs-button crumbs-edit-settings">Edit settings</button>
