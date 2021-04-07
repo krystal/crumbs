317581aed6d30825e34fb0725fc90e260a3fe463
Craig England <craig.england@krystal.uk>
2021-04-07 09:15:19 +0100

    chore: remove unnecessary inline styles from close button on edit screen

--- src/components/editScreen.js
+++ src/components/editScreen.js
@@ -10,7 +10,7 @@ export const editScreen = `
     <div class="crumbs-edit__cta">
       <button class="crumbs-button crumbs-edit-accept">Set cookie preferences</button>
     </div>
-    <button class="crumbs-edit-close" style="appearance:none; border: 0; box-shadow: none; background-color:transparent; cursor: pointer;">
+    <button class="crumbs-edit-close">
       <span class="crumbs__hidden">Close cookie settings</span>
       <svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" aria-hidden="true" focusable="false"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>
     </button>
