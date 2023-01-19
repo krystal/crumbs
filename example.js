1b3e3dd44272e3ecd661531184995722de2c9394
Craig England <craigengland21@gmail.com>
2023-01-19 15:15:18 +0000

    chore: remove babelrc, update npmignore and adjust some basic styles

--- example.js
+++ example.js
@@ -21,21 +21,21 @@ document.addEventListener("DOMContentLoaded", () => {
         identifier: "functional",
         required: true,
         summary:
-          "There are a number of cookies we need to use in order for DeployHQ to work properly. These cannot be disabled.",
+          "There are a number of cookies we need to use in order for out applications to work properly. These cannot be disabled.",
         title: "Functional",
       },
       {
         identifier: "analytics",
         required: false,
         summary:
-          "We use Google Analytics to measure the performance of our website. We do not store any personal data and your IP address is anonymised. Analytics is disabled in the application itself.",
+          "We use analytics to measure the performance of our website. We do not store any personal data and your IP address is anonymised.",
         title: "Analytics",
       },
       {
         identifier: "live_chat",
         required: false,
         summary:
-          "We use a live chat service called Crisp so we can privide support to you where available. Various cookies are stored so chats remain active when you change page.",
+          "We use a live chat service so we can privide support to you where available. Various cookies are stored so chats remain active when you change page.",
         title: "Live Chat",
       },
     ],
